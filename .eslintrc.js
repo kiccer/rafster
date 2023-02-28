ae2cfaedfdd14bb6612f4e948b711e0925d89453
kiccer <1072907338@qq.com>
2023-02-28 22:48:06 +0800

    🐎 ci: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+
+    extends: 'standard',
+
+    overrides: [
+    ],
+
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+
+    include: [
+        'src/**/*.js'
+    ],
+
+    rules: {
+        indent: ['error', 4]
+    }
+}
